36c49696dde25f37070b63b3be40abbc1c4b83f9
Your Name <you@example.com>
2024-03-19 11:45:31 -0700

    dsada

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -10,9 +10,7 @@ def generate_launch_description():
 
 
     # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
-    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
-
-    package_name='articubot_one' #<--- CHANGE ME
+    package_name='articubot_one' 
 
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
@@ -31,6 +29,35 @@ def generate_launch_description():
                         arguments=['-topic', 'robot_description',
                                    '-entity', 'my_bot'],
                         output='screen')
+
+
+####################
+        # Path to the controller configuration YAML file
+    controller_config = os.path.join(
+        get_package_share_directory(package_name),
+        'config',
+        'my_controllers.yaml'
+    )
+
+    # Launch the controller_manager and load the controllers
+    controller_manager_node = Node(
+        package='controller_manager',
+        executable='ros2_control_node',
+        parameters=[{'use_sim_time': True},
+                    controller_config],
+        output='screen',
+    )
+
+    # Optionally, automatically load and start controllers at launch
+    load_and_start_controller = Node(
+        package='controller_manager',
+        executable='spawner.py',
+        arguments=['diff_cont', '--controller-manager', '/controller_manager'],
+    )
+
+
+###############
+
 
 
 
